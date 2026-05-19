d472eee79bc9f05b36d60b4b3e9c54330c4ade34
Aleksandra Chełmińska <aichelminska99@gmail.com>
2026-05-19 23:12:39 +0200

    feat: add HasUuids trait to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
