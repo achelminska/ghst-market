88af895d2f503d883c727da07a3039bb35a2429f
Aleksandra Chełmińska <aichelminska99@gmail.com>
2026-05-19 23:11:12 +0200

    feat: add Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 
+#[Fillable(['name'])]
 class Category extends Model
 {
-    //
+    use HasUuids;
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    protected static function booted(): void
+    {
+        static::creating(fn ($category) => $category->slug = Str::slug($category->name));
+    }
 }
